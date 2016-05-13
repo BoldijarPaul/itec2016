a03a91be93178ab07343903222c7a5d3ba72cb05
Boldijar Paul <boldijar.paul@gmail.com>
2016-05-13 22:05:04 +0300

    updated api

--- app/src/main/java/com/itec/order/data/models/CategoriesResponse.java
+++ app/src/main/java/com/itec/order/data/models/CategoriesResponse.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Created by Paul on 5/13/2016.
  */
-public class CategoryResponse extends BaseResponse {
+public class CategoriesResponse extends BaseResponse {
     @SerializedName("categories")
     public List<Category> categories;
 }
--- app/src/main/java/com/itec/order/data/service/ProductService.java
+++ app/src/main/java/com/itec/order/data/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.itec.order.data.service;
 
+import com.itec.order.data.models.CategoriesResponse;
 import com.itec.order.data.models.ProductsResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by Paul on 5/13/2016.
@@ -12,5 +14,12 @@ public interface ProductService {
 
     @GET("/products")
     Call<ProductsResponse> getProducts();
+
+    @GET("/products/{path}")
+    Call<ProductsResponse> getProducts(@Path("path") int category);
+
+    @GET("/categories")
+    Call<CategoriesResponse> getCategories();
+
 
 }
